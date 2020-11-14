f36a37fee04b9cf2e95fbb7c66a296a85e4439cf
nicodes <nico@ni.codes>
2020-11-13 19:00:08 -0700

    added github link

--- ui/src/components/header/Header.js
+++ ui/src/components/header/Header.js
@@ -1,9 +1,15 @@
+import githubSvg from '../../assets/github.svg'
 import './header.scss'
 
 const Drawer = ({ drawerOpen, setDrawerOpen }) =>
   <header>
     <button onClick={() => setDrawerOpen(!drawerOpen)}>
       {drawerOpen ? '<' : '>'}
+    </button>
+    <button className={'github'}>
+      <a target='_blank' href='https://github.com/nicodes/coviddatemap'>
+        <img src={githubSvg} />
+      </a>
     </button>
   </header>
 
