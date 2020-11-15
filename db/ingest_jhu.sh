cdcc8107e84f94b0bac51e143bc3e4b9c687f426
nicodes <nico@ni.codes>
2020-11-14 19:42:11 -0700

    refactored ingest_jhu to use gnu date

--- db/ingest_jhu.sh
+++ db/ingest_jhu.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
 
 # sh -ac 'source .env && db/ingest_jhu.sh'
-# code from https://stackoverflow.com/questions/4434782/loop-from-start-date-to-end-date-in-mac-os-x-shell-script
 
 PGHOST=$APP_HOST
 PGUSER=$POSTGRES_USER
 PGPASSWORD=$POSTGRES_PASSWORD
 
 url=https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/
-startdate=$(psql -t -c "SELECT GREATEST((SELECT MAX(date) FROM jhu.raw), '2020-03-22')")
-enddate=$(date +'%Y-%m-%d')
-sDateTs=`date -j -f "%Y-%m-%d" $startdate "+%s"`
-eDateTs=`date -j -f "%Y-%m-%d" $enddate "+%s"`
-dateTs=$sDateTs
-offset=86400
+start=$(date -d $(psql -t -c "SELECT GREATEST((SELECT MAX(date) FROM jhu.raw), '2020-03-22')") +%Y%m%d)
 
-while [ "$dateTs" -le "$eDateTs" ]
-do
-  date=`date -j -f "%s" $dateTs "+%m-%d-%Y"`
-  curl ${url}${date}.csv \
-    | python db/parse_jhu.py -d ${date} \
+while [[ $start -le $(date +%Y%m%d) ]]; do 
+  d=$(date -d $start "+%m-%d-%Y")
+  curl ${url}${d}.csv \
+    | python db/parse_jhu.py -d ${d} \
     | psql
-
-  dateTs=$(($dateTs+$offset))
+  start=$(date -d"$start + 1 day" +"%Y%m%d")
 done
 
 echo "Starting ingest..."
