cd6d8d38e87db4d1b9db40ff396d82919e643694
nicodes <nico@ni.codes>
2020-11-14 19:46:10 -0700

    bugfix for ingest_jhu script

--- db/ingest_jhu.sh
+++ db/ingest_jhu.sh
@@ -9,7 +9,7 @@ PGPASSWORD=$POSTGRES_PASSWORD
 url=https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/
 start=$(date -d $(psql -t -c "SELECT GREATEST((SELECT MAX(date) FROM jhu.raw), '2020-03-22')") +%Y%m%d)
 
-while [[ $start -le $(date +%Y%m%d) ]]; do 
+while [ $start -le $(date +%Y%m%d) ]; do 
   d=$(date -d $start "+%m-%d-%Y")
   curl ${url}${d}.csv \
     | python db/parse_jhu.py -d ${d} \
