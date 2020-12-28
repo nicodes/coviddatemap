b7575b8e33691fa7e03ad318456ee1eda4b248a9
Nico Sebastian <35477151+nicodes@users.noreply.github.com>
2020-12-27 21:50:38 -0700

    added donate and refactored header (#3)

--- ui/src/components/header/Header.js
+++ ui/src/components/header/Header.js
@@ -1,17 +1,20 @@
 import hamburgerSvg from '../../assets/hamburger.svg'
 import githubSvg from '../../assets/github.svg'
-import './header.scss'
+import styles from './header.module.scss'
 
 const Drawer = ({ drawerOpen, setDrawerOpen }) =>
-  <header>
-    <button
-      className='hamburger'
-      onClick={() => setDrawerOpen(!drawerOpen)}
-    >
+  <header className={styles.header}>
+    <button className={styles.hamburger} onClick={() => setDrawerOpen(!drawerOpen)}>
       <img src={hamburgerSvg} />
     </button>
     <h1>Covid Date Map</h1>
-    <button className='github'>
+    <button>About</button>
+    <button>
+      <a target='_blank' href='https://www.paypal.com/donate?hosted_button_id=2HHJW2SLA3UVC'>
+        Donate
+      </a>
+    </button>
+    <button className={styles.github}>
       <a target='_blank' href='https://github.com/nicodes/coviddatemap'>
         <img src={githubSvg} />
       </a>
